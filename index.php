18e39f50536ce2c325af000e8ab7cedf5beb7f87
Wesley-4A6B <483509@edu.rocmn.nl>
2018-02-13 15:07:30 +0100

    Added 2 basic files

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Check Please</title>
+    <title>Check Please!</title>
   </head>
   <body>
 <div class="container">
